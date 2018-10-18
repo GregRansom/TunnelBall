77333b040dc9d90aefb9b33651c2532157ca1a4a
GregRansom <gregory@theransoms.co.za>
2018-10-19 01:58:57 +0200

    Build Data
    updated Build of the application

--- Build/Android/src/RasnomIT/TunnelBall/OBBData.java
+++ Build/Android/src/RasnomIT/TunnelBall/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-223435119L // the length of the file in bytes
+223724256L // the length of the file in bytes
 )
 };
 };
